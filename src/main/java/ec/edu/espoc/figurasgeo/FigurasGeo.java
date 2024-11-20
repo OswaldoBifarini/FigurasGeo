8bd8d1b535a10121b58b6d96a2d1b4993317e988
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-20 15:48:00 -0500

    Bienvenidos

--- src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
+++ src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
@@ -12,5 +12,7 @@ public class FigurasGeo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Bienvenidos");
+        
     }
 }
