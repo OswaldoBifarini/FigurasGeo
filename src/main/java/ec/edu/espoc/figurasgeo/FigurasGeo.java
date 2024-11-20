dc7b5725504641adff41050cac42f59f74d02d89
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-20 15:44:13 -0500

    Creación del proyecto

---
+++ src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoc.figurasgeo;
+
+/**
+ *
+ * @author DELL
+ */
+public class FigurasGeo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
