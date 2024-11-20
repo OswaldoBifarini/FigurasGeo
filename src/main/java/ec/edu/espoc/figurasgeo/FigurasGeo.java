d5dccc8e7986baba0ec123820e58488e8f388b89
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-11-20 16:02:49 -0500

    Modificación de texto

--- src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
+++ src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
@@ -13,6 +13,7 @@ public class FigurasGeo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Bienvenidos");
+        System.out.println("Hola soy Jennifer");
         
     }
 }
