6069d513fbf1d47c8bbae7f8341c5d203359a5f0
OswaldoBifarini <jgbifarini@gmail.com>
2024-11-21 13:53:36 -0500

    Practica 21/11/2024

--- src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
+++ src/main/java/ec/edu/espoc/figurasgeo/FigurasGeo.java
@@ -11,8 +11,7 @@ package ec.edu.espoc.figurasgeo;
 public class FigurasGeo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Bienvenidos");
+        System.out.println("");
         
     }
 }
